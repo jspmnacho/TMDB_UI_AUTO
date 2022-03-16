8370183a12973309355dd1e1e354a66eb4c9f3f8
JuanPuerto <juan.puerto086@gmail.com>
2022-03-16 16:06:36 -0500

    Implementación de reportes
    Ultimo commit
    Implementación de reportes
    Solucion de Errores

--- src/test/java/pages/FilterPage.java
+++ src/test/java/pages/FilterPage.java
@@ -5,10 +5,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class FilterPage extends BasePage{
